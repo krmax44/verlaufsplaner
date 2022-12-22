14ebcce159c12e0bf0af2391f71bc45ee9cfb574
krmax44 <hi@krmax44.de>
2022-12-22 15:17:34 +0100

    ♿️ fix keyboard navigation on menu items

--- src/components/MenuItem.vue
+++ src/components/MenuItem.vue
@@ -3,10 +3,12 @@ import { MenuItem } from '@headlessui/vue';
 </script>
 
 <template>
-  <MenuItem
-    as="button"
-    class="flex w-full items-center rounded-sm px-2 py-2 text-sm hover:bg-gray-200 active:bg-gray-300 dark:hover:bg-gray-800 dark:active:bg-gray-900"
-  >
-    <slot />
+  <MenuItem v-slot="{ active }">
+    <button
+      class="flex w-full items-center rounded-sm px-2 py-2 text-sm active:bg-gray-300 dark:active:bg-gray-900"
+      :class="active && 'bg-gray-200 dark:bg-gray-800'"
+    >
+      <slot />
+    </button>
   </MenuItem>
 </template>
