807fda71e1f4e7507b6435d3181c6489ae94c366
Max <hi@krmax44.de>
2023-11-08 16:34:02 +0100

    🩹 back to github pages domain

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import IconsResolver from 'unplugin-icons/resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/verlaufsplaner/',
   plugins: [
     Vue(),
     Components({
